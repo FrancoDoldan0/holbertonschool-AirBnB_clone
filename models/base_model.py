a4f018b0c2fc93426400e65d8a2f1b67dcb90d48
FrancoDoldan0 <francodoldan23@gmail.com>
2024-02-27 09:27:42 -0800

    add task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,25 @@
 import uuid
 from datetime import datetime
 
+# Modelo base
+
+
 class BaseModel(self):
+    """class"""
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now
         self.update_at = self.created_at
 
     def __str__(self):
-        return(f"[{self.__class__.name___}] ({self.id}) {self.__dict__}")
+        return (f"[{self.__class__.name___}] ({self.id}) {self.__dict__}")
+
+    def save(self):
+        self.update_at = self.created_at
+
+    def to_dict(self):
+        ob_dict = self.__dict__.copy()
+        ob_dict["__class__"] = self.__class__.__name__
+        ob_dict["create_at"] = self.created_at.isoformat()
+        ob_dict["update_at"] = self.update_at.isoformat()
+        return ob_dict
