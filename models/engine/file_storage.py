299bbf30e370a1365c0b10cf1e5c06685bf166b6
FrancoDoldan0 <francodoldan23@gmail.com>
2024-02-27 08:41:44 -0800

    add task 3

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,1 @@
-import uuid
-from datetime import datetime
 
-class BaseModel(self):
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now
-        self.update_at = self.created_at
-
-    def __str__(self):
-        return(f"[{self.__class__.name___}] ({self.id}) {self.__dict__}")
