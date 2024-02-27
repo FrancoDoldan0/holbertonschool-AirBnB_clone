4dd4cba6247d2e3e89d9cc4cb180fa3ba05fbca7
FrancoDoldan0 <francodoldan23@gmail.com>
2024-02-27 10:01:51 -0800

    add task 3 fixed

--- models/base_model.py
+++ models/base_model.py
@@ -9,17 +9,17 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.update_at = self.created_at
+        self.updated_at = self.created_at
 
     def __str__(self):
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         ob_dict = self.__dict__.copy()
         ob_dict["__class__"] = self.__class__.__name__
-        ob_dict["create_at"] = self.created_at.isoformat()
-        ob_dict["update_at"] = self.update_at.isoformat()
+        ob_dict["created_at"] = self.created_at.isoformat()
+        ob_dict["updated_at"] = self.updated_at.isoformat()
         return ob_dict
