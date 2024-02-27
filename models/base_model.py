a5d17086fea29db87779e04d6d95d35fceef7f0d
FrancoDoldan0 <francodoldan23@gmail.com>
2024-02-27 09:44:26 -0800

    task fixed

--- models/base_model.py
+++ models/base_model.py
@@ -8,14 +8,14 @@ class BaseModel:
     """class"""
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now
+        self.created_at = datetime.now()
         self.update_at = self.created_at
 
     def __str__(self):
         return (f"[{self.__class__.name___}] ({self.id}) {self.__dict__}")
 
     def save(self):
-        self.update_at = datetime.now
+        self.update_at = datetime.now()
 
     def to_dict(self):
         ob_dict = self.__dict__.copy()
