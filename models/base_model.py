a4fdf8b98b5fa3aa3a0a284e366180f8c835789a
FrancoDoldan0 <francodoldan23@gmail.com>
2024-02-28 08:17:20 -0800

    add task 3 fixed

--- models/base_model.py
+++ models/base_model.py
@@ -6,21 +6,21 @@ from datetime import datetime
 
 class BaseModel:
     """class"""
-    def __init__(self):
+    def _init_(self):
         """base"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
-    def __str__(self):
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+    def _str_(self):
+        return (f"[{self._class.name}] ({self.id}) {self.dict_}")
 
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        ob_dict = self.__dict__.copy()
-        ob_dict["__class__"] = self.__class__.__name__
+        ob_dict = self._dict_.copy()
+        ob_dict["_class"] = self.__class__.__name__
         ob_dict["created_at"] = self.created_at.isoformat()
         ob_dict["updated_at"] = self.updated_at.isoformat()
         return ob_dict
