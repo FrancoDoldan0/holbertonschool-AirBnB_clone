4eaaa855d3f889bf6032dccdee3c7a6893b6879c
FrancoDoldan0 <francodoldan23@gmail.com>
2024-02-28 08:30:02 -0800

    add task 4

--- models/base_model.py
+++ models/base_model.py
@@ -5,18 +5,20 @@ from datetime import datetime
 
 
 class BaseModel:
-    """class"""
-    def __init__(self):
-        """base"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-
-    def __str__(self):
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
-
-    def save(self):
-        self.updated_at = datetime.now()
+    def _init_(self, *args, **kwargs):
+        """BaseModel"""
+        if len(kwargs) > 0:
+            for k in kwargs:
+                if k is not "_class_":
+                    if k is "updated_at" or k is "created_at":
+                        setattr(self, k, datetime.
+                                strptime(kwargs[k], "%Y-%m-%d %H:%M:%S.%f"))
+                    else:
+                        setattr(self, k, kwargs[k])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def to_dict(self):
         ob_dict = self.__dict__.copy()
