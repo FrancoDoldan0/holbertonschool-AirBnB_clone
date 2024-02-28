807855691e6062074e90515c97c74f9a32f6f46e
FrancoDoldan0 <135631203+FrancoDoldan0@users.noreply.github.com>
2024-02-28 11:09:40 -0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,18 @@
 import uuid
 from datetime import datetime
 
-# Modelo base
+#class
 
 
-class BaseModel:
-    """A base model representing common attributes and methods for other models."""
-
+class BaseModel(self):
+    """def class"""
     def __init__(self):
-
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.created_at = datetime.now
+        self.update_at = self.created_at
 
     def __str__(self):
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+        return(f"[{self.__class__.name___}] ({self.id}) {self.__dict__}")
 
     def save(self):
         self.updated_at = datetime.now()
