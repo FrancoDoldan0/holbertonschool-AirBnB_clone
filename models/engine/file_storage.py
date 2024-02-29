c843ad83f7023c207ec81be48a5a10eac1cadea3
FrancoDoldan0 <francodoldan23@gmail.com>
2024-02-29 09:23:26 -0800

    add task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,5 @@
 import json
+from models.base_model import BaseModel
 import os
 
 
@@ -22,13 +23,13 @@ class FileStorage:
         with open(self.__file_path, "w") as file:
             json.dump(obj_dict, file)
 
-        def reload(self):
-            if os.path.exists(self.__file_path):
-                with open(self.__file_path, "r") as f:
-                    objs = json.load(f)
-            if objs:
-                for obj in objs.values():
-                    class_name = obj['__class__']
-                    actual_class_type = eval(class_name)
-                    deserialized_object = actual_class_type(**obj)
-                    self.new(deserialized_object)
+    def reload(self):
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, "r") as f:
+                objs = json.load(f)
+        if objs:
+            for obj in objs.values():
+                class_name = obj['__class__']
+                actual_class_type = eval(class_name)
+                deserialized_object = actual_class_type(**obj)
+                self.new(deserialized_object)
