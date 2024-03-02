fbdc7454324d0324b372030467820776c45bc0d7
FrancoDoldan0 <francodoldan23@gmail.com>
2024-03-02 14:04:29 -0800

    add show

--- console.py
+++ console.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """entry point of the command interpreter"""
 import cmd
+from models.user import User
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.review import Review
+from models.place import Place
 
 
 class HBNBCommand(cmd.Cmd):
@@ -16,46 +22,70 @@ class HBNBCommand(cmd.Cmd):
         "Review"
     }
 
-    def do_quit(self, args):
+    def do_quit(self, name_arg):
         """Exit the program"""
         return True
 
-    def do_EOF(self, args):
+    def do_EOF(self, name_arg):
         """Exit the program"""
         return True
 
-    def do_help(self, arg):
+    def do_help(self, arg_passed):
         """help"""
-        cmd.Cmd.do_help(self, arg)
+        cmd.Cmd.do_help(self, arg_passed)
 
     def emptyline(self):
         """empty line shouldn’t execute anything"""
         pass
 
-    def do_create(self, arg):
-        """""Creates a new instance of a class"""
-        args = arg.split()
+    def do_create(self, arg_passed):
+        """
+        The code first splits the user input into words,
+        checks if the class name is provided and if the class exists.
+        Then, it creates an instance of the specified class, saves it,
+        and finally prints the ID of the created instance.
+        """
+        name_arg = arg_passed.split()
 
-        if len(args) == 0:
+        if len(name_arg) == 0:
             print("** class name missing **")
             return
 
-        class_name = args[0]
+        class_name = name_arg[0]
 
-        if class_name not in HBNBCommand().model_classes:
+        if class_name not in self.model_classes:
             print("** class doesn't exist **")
             return
 
         from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.amenity import Amenity
-        from models.city import City
-        from models.review import Review
-        from models.place import Place
-        new_instance = eval(f"{arg}()")
+        new_instance = eval(f"{arg_passed}()")
         new_instance.save()
         print(new_instance.id)
+
+    def do_show(self, arg_passed):
+
+        """checks if a class name was passed"""
+        name_arg = arg_passed.split()
+        if len(name_arg) == 0:
+            print("** class name missing **")
+            return
+
+        """check if the class name is in the class dictionary"""
+        class_name = name_arg[0]
+        if class_name not in self.model_classes:
+            print("** class doesn't exist **")
+            return
+
+        """check if the id class was passed"""
+        if len(name_arg) < 2:
+            print("** instance id missing **")
+            return
+
+        """check if the name and id are not in the class dictionary"""
+        from models import storage
+        if f"{name_arg[0]}.{name_arg[1]}" not in storage.all():
+            print("** no instance found **")
+            return
 
 
 if __name__ == '__main__':
