cce3ddd8b5149fb6e79128afed66b27d2af59ba2
FrancoDoldan0 <francodoldan23@gmail.com>
2024-03-03 09:32:23 -0800

    add destroy and all method

--- console.py
+++ console.py
@@ -88,7 +88,69 @@ class HBNBCommand(cmd.Cmd):
             return
         else:
             print(storage.all()[f"{name_arg[0]}.{name_arg[1]}"])
+            return """if name and id exist"""
+
+    def do_destroy(self, arg_passed):
+        """coments"""
+        name_arg = arg_passed.split()
+
+        if len(name_arg) == 0:
+            print("** class name missing **")
             return
+
+        class_name = name_arg[0]
+        if class_name not in self.model_classes:
+            print("** class doesn't exist **")
+            return
+
+        """check if the id class was passed"""
+        if len(name_arg) == 1:
+            print("** instance id missing **")
+            return
+
+        from models import storage
+        if f"{name_arg[0]}.{name_arg[1]}" not in storage.all():
+            print("** no instance found **")
+            return
+        else:
+            del storage.all()[f"{name_arg[0]}.{name_arg[1]}"]
+            storage.save()
+            return
+
+    def do_all(self, args_passed):
+        """all method"""
+        from models import storage
+        """
+        The do_all method prints instances from storage based on the
+        provided class. If no class is given, it prints all instances.
+        If an invalid class is provided, it shows an error message.
+        If a valid class is given, it prints instances belonging to that class
+        """
+        name_args = args_passed.split()
+        instances = storage.all()
+
+        if not name_args:
+            print([str(instance) for instance in instances.values()])
+        elif name_args[0] not in self.model_classes:
+            print("** class doesn't exist **")
+        else:
+            """In this else block, the first argument(which is the class name)
+            is taken and used to search for instances in the storage.
+            All instances in the storage are iterated over,
+            and the class name of each instance is compared to the
+            provided class name.
+            If they match, the instance is converted to a string and
+            added to a list.
+            Finally, this list of instances is printed.
+            """
+            class_name = name_args[0]
+            matching_instances = []
+            for key, instance in instances.items():
+                class_from_key = key.split('.')[0]
+                if class_from_key == class_name:
+                    instance_str = str(instance)
+                    matching_instances.append(instance_str)
+            print(matching_instances)
 
 
 if __name__ == '__main__':
