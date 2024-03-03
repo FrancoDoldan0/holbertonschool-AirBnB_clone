dc38bd0f1bfe46ab7fcff12706aa9e93144c6658
pablosalina25 <casuarioventas@gmai.com>
2024-03-03 13:13:41 +0000

    test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -47,11 +47,29 @@ class TestBaseModel(unittest.TestCase):
 
         self.assertNotEqual(initial_updated_at, base_model_instance.updated_at)
 
-        def test_str(self):
-            """test str """
+    def test_str(self):
+        """test str """
         instance = "[BaseModel] ({}) {}".format(
             self.base_model.id, self.base_model.__dict__)
         self.assertEqual(str(self.base_model), instance)
+
+    def test_to_dict_conversion(self):
+        """
+        Test the conversion of the object to a dictionary.
+        """
+        instance = BaseModel()
+        instance.name = 'Diego'
+        instance.my_number = 22
+
+        expected_dict = {'id': instance.id, 'created_at': instance.created_at,
+                                            'updated_at': instance.updated_at,
+                                            'name': 'Diego', 'my_number': 22}
+
+        result_dict = {'id': instance.id,   'created_at': instance.created_at,
+                                            'updated_at': instance.updated_at,
+                                            'name': 'Diego', 'my_number': 22}
+
+        self.assertDictEqual(expected_dict, result_dict)
 
 
 if __name__ == '__main__':
